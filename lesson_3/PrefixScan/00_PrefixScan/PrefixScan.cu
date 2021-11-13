387ceb3adb2392ffe49598f84dbba439e6813add
Filippo Nevi <filipponevi@protonmail.com>
2021-11-13 15:41:00 +0100

    Update PrefixScan.cu

--- lesson_3/PrefixScan/00_PrefixScan/PrefixScan.cu
+++ lesson_3/PrefixScan/00_PrefixScan/PrefixScan.cu
@@ -37,9 +37,9 @@ void printArray(int* Array, int N, const char str[] = "") {
 
 int main() {
 	const int blockDim = BLOCK_SIZE;
-	//const int N = BLOCK_SIZE * 131072;
+	const int N = BLOCK_SIZE * 131072;
 	//const int N = BLOCK_SIZE * 65536;
-	const int N = BLOCK_SIZE * 8192;
+	
     // ------------------- INIT ------------------------------------------------
 
     // Random Engine Initialization
