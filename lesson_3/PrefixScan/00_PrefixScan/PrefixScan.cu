8faa1d1e60dd9c008ce7cba2649fac613052099e
Filippo Nevi <filipponevi@protonmail.com>
2021-11-13 18:09:04 +0100

    Update PrefixScan.cu

--- lesson_3/PrefixScan/00_PrefixScan/PrefixScan.cu
+++ lesson_3/PrefixScan/00_PrefixScan/PrefixScan.cu
@@ -12,7 +12,7 @@ __global__ void PrefixScan(int* VectorIN, int N) {
 	int globalIndex = blockIdx.x*BLOCK_SIZE + threadIdx.x;
 	int offset = 1;
 	for(int level = 1; level <= N; level *= 2) {	
-		if (globalIndex >= offset)
+		if (globalIndex >= offset && globalIndex < N)
 			VectorIN[globalIndex] = VectorIN[globalIndex - offset] + VectorIN[globalIndex];
 		__syncthreads();
 		offset *= 2;
