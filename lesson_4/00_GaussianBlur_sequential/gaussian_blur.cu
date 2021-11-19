b98e6c2ffb2821851f6abc7144125951bb63ef1c
Filippo Nevi <filipponevi@protonmail.com>
2021-11-19 16:42:33 +0100

    Update gaussian_blur.cu

--- lesson_4/00_GaussianBlur_sequential/gaussian_blur.cu
+++ lesson_4/00_GaussianBlur_sequential/gaussian_blur.cu
@@ -63,7 +63,7 @@ int main() {
             h_MatrixA[i*width + j] = distribution(generator);
 
 
-    for (int y = 0; y < height; ++y) {
+    /*for (int y = 0; y < height; ++y) {
         for (int x = 0; x < width; ++x) {
             for (int channel = 0; channel < 3; ++channel) {
                 float pixel_value = 0;
@@ -79,7 +79,7 @@ int main() {
             // no transparency
             h_MatrixB[(y * width + x) * 4 + 3] = (unsigned char)255;
         }
-    }
+    }*/
 
     std::cout<<"Starting computation on DEVICE "<<std::endl;
 
