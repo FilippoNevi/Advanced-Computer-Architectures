d181bf8eddb17fc6717d1aa7e1ea812e00bfdbe1
Filippo Nevi <filipponevi@protonmail.com>
2021-11-19 16:41:22 +0100

    Update gaussian_blur.cu

--- lesson_4/00_GaussianBlur_sequential/gaussian_blur.cu
+++ lesson_4/00_GaussianBlur_sequential/gaussian_blur.cu
@@ -21,7 +21,7 @@ float mask[N][N] = {
     {0.0030, 0.0133, 0.0219, 0.0133, 0.0030}
 };
 
-__global__ void GaussianBlur(int* MatrixA, int* MatrixB, float mask[N][N], int N, int height, int width) {
+__global__ void GaussianBlur(int* MatrixA, int* MatrixB, float* mask, int N, int height, int width) {
     for (int y = 0; y < height; ++y) {
         for (int x = 0; x < width; ++x) {
             for (int channel = 0; channel < 3; ++channel) {
@@ -30,7 +30,7 @@ __global__ void GaussianBlur(int* MatrixA, int* MatrixB, float mask[N][N], int N
                     for (int v = 0; v < N; ++v) {
                         int new_x = min(width, max(0, x+u-N/2));
                         int new_y = min(height, max(0, y+v-N/2));
-                        pixel_value += mask[v*N+u]*MatrixA[(new_y*width+new_x)*4+channel];
+                        pixel_value += mask[v*N+u] * MatrixA[(new_y*width+new_x)*4+channel];
                     }
                 }
                 MatrixB[(y*width+x)*4+channel] = (unsigned char)pixel_value;
@@ -71,7 +71,7 @@ int main() {
                     for (int v = 0; v < N; ++v) {
                         int new_x = min(width, max(0, x+u-N/2));
                         int new_y = min(height, max(0, y+v-N/2));
-                        pixel_value += mask[v*N+u]*h_MatrixA[(new_y * width + new_x) * 4 + channel];
+                        pixel_value += mask[v*N+u] * h_MatrixA[(new_y * width + new_x) * 4 + channel];
                     }
                 }
                 h_MatrixB[(y * width + x) * 4 + channel] = (unsigned char)pixel_value;
@@ -84,7 +84,7 @@ int main() {
     std::cout<<"Starting computation on DEVICE "<<std::endl;
 
     dev_TM.start();
-    GaussianBlur<<<DIV(N, BLOCK_SIZE), BLOCK_SIZE>>>(d_MatrixA, d_MatrixB, mask, N, height, width);
+    GaussianBlur<<<DIV(N, BLOCK_SIZE), BLOCK_SIZE>>>(d_MatrixA, d_MatrixB, &mask, N, height, width);
 
     dev_TM.stop();
     dev_time = dev_TM.duration();
